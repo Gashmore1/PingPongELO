0253915cc2f9dc23c56c6451bc0f5f30577739c8
George Ashmore <georgeashmore1@gmail.com>
2024-02-25 23:56:47 +0000

    FIX perfomance issues by adding postgres

--- index.py
+++ index.py
@@ -1,23 +1,40 @@
-from pyspark.sql.types import DoubleType, LongType, StructType, StructField, StringType, IntegerType
-from pyspark.sql import SparkSession
-from pyspark.conf import SparkConf
+import pandas as pd
+import psycopg2
+
+from urllib.parse import quote_plus
+from sqlalchemy import create_engine
+import sqlalchemy
 
 from flask import redirect, render_template, request, url_for, abort, Flask
 
 import json
 import numpy as np
 
+from pprint import pprint
+
 # If this was ever to become GLICKO ranking method time since last game would need to be used
 from datetime import datetime
 
 app = Flask(__name__)
 LEARNING_RATE = 50
-TABLE = "pingpong.ratings"
 
 with open("conf.json", "r") as f:
-    configuration = json.load(f)
+    pg_configuration = json.load(f)
 
-spark_conf = SparkConf().setAll(configuration)
+DB_USER_NAME = pg_configuration["username"]
+DB_PASSWORD = quote_plus(pg_configuration["password"])
+DB_URL = pg_configuration["url"]
+DB_PORT = pg_configuration["port"]
+DB_NAME = pg_configuration["dbname"]
+DB_TABLE = pg_configuration["table"]
+
+engine = create_engine(f"postgresql+psycopg2://{DB_USER_NAME}:{DB_PASSWORD}@{DB_URL}:{DB_PORT}/{DB_NAME}")
+
+schema = [
+    "name",
+    "rating",
+    "game_number"
+    ]
 
 def expected_result(ranking_a, ranking_b):
     expected_result_a = 1/(1 + np.power(10,(ranking_b-ranking_a)/100))
@@ -34,33 +51,30 @@ def new_rankings(ranking_a, ranking_b, change_rate, win):
     return results
 
 def get_player_score(name, df):
-    user_details = df.where(df.name == name).sort(df.game_number, ascending=False).first()
-    if user_details:
+    if name in df.name:
+        filter_name = df.name == name
+        user_details = df.where(filter_name).sort_values("game_number", ascending=False).iloc[0]
         return user_details.rating
     else:
         return 100
 
 def get_player_game_number(name, df):
-    user_details =  df.where(df.name == name).sort(df.game_number, ascending=False).first()
-    if user_details:
+    if name in df.name.values:
+        filter_name = df.name == name
+        user_details =  df.where(filter_name).sort_values("game_number", ascending=False).iloc[0]
         return user_details.game_number
     else:
         return 0
 
 
-def add_player_score(name, rating, df, session):
-    schema = StructType([
-        StructField("name", StringType(), False),
-        StructField("rating", DoubleType(), False),
-        StructField("game_number", IntegerType(), False)]
-        )
+def add_player_score(name, rating, df):
 
     game_number = get_player_game_number(name, df) + 1
 
-    df = df.unionAll(session.createDataFrame([{"name": name, "rating": float(rating), "game_number": game_number}], schema=schema))
-    return df
 
-def add_game_result(player_a, player_b, player_a_score, player_b_score, df, session):
+    return pd.DataFrame([{"name": name, "rating": float(rating), "game_number": game_number}], columns=schema)
+
+def add_game_result(player_a, player_b, player_a_score, player_b_score, df):
     player_a_ranking = get_player_score(player_a, df)
     player_b_ranking = get_player_score(player_b, df)
 
@@ -71,37 +85,26 @@ def add_game_result(player_a, player_b, player_a_score, player_b_score, df, sess
 
     new_player_a_ranking, new_player_b_ranking = new_rankings(player_a_ranking, player_b_ranking, LEARNING_RATE, score_calculation)
 
-    df = add_player_score(player_a, new_player_a_ranking, df, session)
-    df = add_player_score(player_b, new_player_b_ranking, df, session)
+    output_df = add_player_score(player_a, new_player_a_ranking, df)
+    output_df.loc[len(output_df)] = add_player_score(player_b, new_player_b_ranking, df).iloc[0]
 
-    return df
+    return output_df
 
 def leader_board(data):
-    leader_board = data.select(data.name, data.rating, data.game_number).sort(data.game_number, ascending=False).dropDuplicates(["name"]).sort(data.rating, ascending=False).collect()
-    return [{"place": index, "name": player.name} for index, player in enumerate(leader_board)]
+    leader_board = data.sort_values("game_number", ascending=False).drop_duplicates(subset=["name"]).sort_values("rating", ascending=False)
+    return [{"place": index+1, "name": leader_board.name.values[index]} for index in range(len(leader_board))]
 
 @app.route("/")
 def index():
-    spark = SparkSession.builder.appName("PingPongELO").config(conf=spark_conf).getOrCreate()
     """Input form for new scores along with a leader board."""
-    # Connection details will be in spark-sumbit command
-    table_exists = spark.catalog.tableExists(TABLE)
-
-    if table_exists:
-        ratings = spark.read.table(TABLE)
-    else:
-        schema = StructType([
-            StructField("name", StringType(), False),
-            StructField("rating", DoubleType(), False),
-            StructField("game_number", IntegerType(), False)
-            ]
-        )
-        ratings = spark.createDataFrame([],schema=schema)
+    with engine.connect() as conn:
+        try:
+            ratings = pd.read_sql(sql=f"select name, rating, game_number from {DB_TABLE}", con=conn, columns=schema)
+        except sqlalchemy.exc.ProgrammingError:
+            ratings = pd.DataFrame([], columns=schema)
 
     # Dataframe returned from spark
     players = leader_board(ratings)
-
-    spark.stop()
 
     return render_template("index.html", players=players)
 
@@ -109,21 +112,11 @@ def index():
 @app.route("/result", methods=("GET", "POST"))
 def create():
     """Save result."""
-    spark = SparkSession.builder.appName("PingPongELO").config(conf=spark_conf).getOrCreate()
-
-    # Connection details will be in spark-sumbit command
-    table_exists = spark.catalog.tableExists(TABLE)
-
-    if table_exists:
-        ratings = spark.read.table(TABLE)
-    else:
-        schema = StructType([
-            StructField("name", StringType(), False),
-            StructField("rating", DoubleType(), False),
-            StructField("game_number", IntegerType(), False)
-            ]
-        )
-        ratings = spark.createDataFrame([],schema=schema)
+    with engine.connect() as conn:
+        try:
+            ratings = pd.read_sql(sql=f"select name, rating, game_number from {DB_TABLE}", con=conn, columns=schema)
+        except sqlalchemy.exc.ProgrammingError:
+            ratings = pd.DataFrame([], columns=schema)
 
     if request.method == 'POST':
         player_1 = request.form["player_1"]
@@ -137,9 +130,10 @@ def create():
         score_2 = int(request.args.get("score_2"))
 
     #Use method to calculate score to append new scores
-    scores = add_game_result(player_1, player_2, score_1, score_2, ratings, spark)
+    ratings = add_game_result(player_1, player_2, score_1, score_2, ratings)
 
-    print(scores.collect())
+    with engine.connect() as conn:
+        ratings.to_sql(DB_TABLE, con=conn, if_exists='append')
 
     return redirect(url_for("index"))
 
